5f89ffea086afcf4960db63b99ba9b48314e7137
Todd Perry <up612136@myport.ac.uk>
2013-04-28 01:28:34 +0100

    implemented pending review count

--- WEBSCRP/612136/scripts/get_poor_reviews.php
+++ WEBSCRP/612136/scripts/get_poor_reviews.php
@@ -12,45 +12,62 @@
 				include_once "mysql.php";
 				
 					function getLatestPoorComment($con){
-					$query = "SELECT * FROM `comments`";
-					if (!$con){
-						die('Could not connect: ' . mysql_error());
-					}
-					if (mysql_query($query ,$con)){
-						$output = (mysql_query($query ,$con));
-						while($row = mysql_fetch_array($output)){
-							//echo NULL == 0;
-							if ($row['rating'] != -1 && $row['rating'] < 4 && $row['replied'] !== "0"){
-								// not -1, valid comment
-								$itemID = $row['itemID'];
-								$itemName = getData("SELECT * FROM `items` WHERE `itemID` = '$itemID'", $con)[0];
-								echo "<div id='poorComment'>";
-								echo "<input type='hidden' id='itemID' value='$itemID'>";
-								echo "<input type='hidden' id='OP' value='" . $row['posterName'] . "'>";
-								echo "<input type='hidden' id='OPID' value='" . $row['commentID'] . "'>";
-								echo "<p>Item Name: '" . $itemName . "'</p>";
-								echo "<p>Poster Name: '" . $row['posterName'] . "'</p>";
-								echo "<p>Comment: '" . $row['commentBody'] . "'</p>";
-								echo "<p>Rating: " . $row['rating'] . "/10</p>";
-								
-								echo "<p></p>";
-								
-								echo "<div id='replyForm'>";
-								echo "<p>Reply:</p>";
-								echo "<textarea cols='26' rows='5' id='replyText'></textarea>";
-								echo "<p></p>";
-								echo "<button onclick='return uploadReply();'>Reply</button>";
-								echo "<button onclick='return ignore();'>Ignore</button>";
-								echo "</div>";
-								echo "</div>";
-								return false;
-							}
+						$query = "SELECT * FROM `comments`";
+						if (!$con){
+							die('Could not connect: ' . mysql_error());
 						}
-						echo "<div id='poorComments'>";
-						echo "There Doesn't Appear To Be Any Recent Poor Comments";	
-						echo "</div>";		
+						if (mysql_query($query ,$con)){
+							$output = (mysql_query($query ,$con));
+							while($row = mysql_fetch_array($output)){
+								//echo NULL == 0;
+								if ($row['rating'] != -1 && $row['rating'] < 4 && $row['replied'] !== "0"){
+									// not -1, valid comment
+									$itemID = $row['itemID'];
+									$itemName = getData("SELECT * FROM `items` WHERE `itemID` = '$itemID'", $con)[0];
+									echo "<div id='poorComment'>";
+									echo "<input type='hidden' id='itemID' value='$itemID'>";
+									echo "<input type='hidden' id='OP' value='" . $row['posterName'] . "'>";
+									echo "<input type='hidden' id='OPID' value='" . $row['commentID'] . "'>";
+									echo "<p>Item Name: '" . $itemName . "'</p>";
+									echo "<p>Poster Name: '" . $row['posterName'] . "'</p>";
+									echo "<p>Comment: '" . $row['commentBody'] . "'</p>";
+									echo "<p>Rating: " . $row['rating'] . "/10</p>";
+									
+									echo "<p></p>";
+									
+									echo "<div id='replyForm'>";
+									echo "<p>Reply:</p>";
+									echo "<textarea cols='26' rows='5' id='replyText'></textarea>";
+									echo "<p></p>";
+									echo "<button onclick='return uploadReply();'>Reply</button>";
+									echo "<button onclick='return ignore();'>Ignore</button>";
+									echo "</div>";
+									return false;
+								}
+							}
+							echo "There Doesn't Appear To Be Any Recent Poor Comments";	
+						}
+					
 					}
 					
+					
+					function getNumberOfPoorComments($con){
+						$query = "SELECT * FROM `comments`";
+						if (!$con){
+							die('Could not connect: ' . mysql_error());
+						}
+						if (mysql_query($query ,$con)){
+							$output = (mysql_query($query ,$con));
+							$count = 0;
+							while($row = mysql_fetch_array($output)){
+								//echo NULL == 0;
+								if ($row['rating'] != -1 && $row['rating'] < 4 && $row['replied'] !== "0"){
+									$count++;
+								}
+							}
+							return $count;
+						}
+						
 					}
 				
 				
@@ -59,6 +76,13 @@
 					executeQuery($query, $con);
 					
 					getLatestPoorComment($con);
+					
+					$pending = getNumberOfPoorComments($con);
+					if ($pending != 0){
+						echo "<p id='pending' class='charRemaining'>$pending Comment(s) Pending</p>";
+					}
+					
+					echo "</div>";		
 				
 					mysql_close($con);
 				
